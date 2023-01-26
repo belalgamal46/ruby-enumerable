dfefe86ef0244f492f289dcb120320987fc9f62b
Ibrahim Suhail Al-Yousefi <cluab11222@gmail.com>
2023-01-26 16:23:13 +0200

    add readme file content and tests for enumerable

--- main.rb
+++ main.rb
@@ -11,3 +11,17 @@ class MyList
     @list.each { |x| yield x }
   end
 end
+
+
+list = MyList.new(1, 2, 3, 4)
+
+
+list.all? {|e| e < 5}
+
+list.all? {|e| e > 5}
+
+list.any? {|e| e == 2}
+
+list.any? {|e| e == 5}
+
+list.filter {|e| e.even?}
